c80061c4d575ec1a6e26f5712ccdcaf4f23ea7e7
Andras <andras@andrasq.com>
2015-09-18 13:14:38 -0400

    oops, export QBuffer from source

--- qbuffer.js
+++ qbuffer.js
@@ -156,6 +156,7 @@ QBuffer.prototype = {
     // returns the first chunk, now larger, or null if no data
     _concat:
     function _concat( bound ) {
+        if (this.length < bound) return null
         var chunks = this.chunks
         var len = 0, nchunks
         if (chunks.length < bound) return false
@@ -181,31 +182,4 @@ QBuffer.prototype = {
 }
 
 
-// quickest:
-/***
-
-var b = new QBuffer
-
-var s200 = new Array(200).join('x') + "\n"
-var s20k = new Array(20001).join(s200)
-
-b.write("line1\nline2\nline3\nline4\n")
-b.write(s20k)
-b.write(s20k)
-b.write(s20k)
-b.write(s20k)
-b.write(s20k)
-console.log(b.getline())
-//b.setEncoding('utf8')
-console.log(b.getline())
-console.log(b.getline())
-console.log(b.getline())
-var t1 = Date.now()
-for (var i=0; i<100000; i++) { var line = b.getline(); if (line.length !== 200) console.log("FAIL") }
-var t2 = Date.now()
-console.log("100k getline in %d", t2 - t1)
-// 800k 200B lines per second, not quite as fast as qfgets (utf8)
-// 1.2m 200B buffers per second, faster than qfgets (binary)
-console.log(b)
-
-/***/
+module.exports = QBuffer
